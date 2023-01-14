054e3413d869f466837f4cbf15dec83ef088fb43
Bennie Draaijer <benniedraaijer@icloud.com>
2023-01-14 10:40:24 +0100

    Update Main.tsx

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,5 +1,5 @@
 import { Flex } from '@chakra-ui/react'
-import { motion } from 'framer-motion'
+import { motion } from 'framer-motion/dist/index'
 import React from 'react'
 
 const FlexMotion = motion(Flex)
